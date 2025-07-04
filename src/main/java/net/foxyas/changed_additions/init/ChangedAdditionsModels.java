b45ebc8157d963c0c223db43301c9f937c474902
Foxyas <CarlosDeck9474@gmail.com>
2025-07-04 17:33:48 -0300

    Update ChangedAdditionsModels.java

--- src/main/java/net/foxyas/changed_additions/init/ChangedAdditionsModels.java
+++ src/main/java/net/foxyas/changed_additions/init/ChangedAdditionsModels.java
@@ -1,7 +1,6 @@
 package net.foxyas.changed_additions.init;
 
 import net.foxyas.changed_additions.client.models.*;
-import net.foxyas.changed_additions.client.models.accessories.models.*;
 import net.foxyas.changed_additions.client.models.armors.DarkLatexCoatModel;
 import net.foxyas.changed_additions.client.models.armors.SkinLayerModel;
 import net.minecraftforge.api.distmarker.Dist;
@@ -18,14 +17,6 @@ public class ChangedAdditionsModels {
         event.registerLayerDefinition(LatexSnowFoxFemaleModel.LAYER_LOCATION, LatexSnowFoxFemaleModel::createBodyLayer);
         event.registerLayerDefinition(LatexKitsuneMaleModel.LAYER_LOCATION, LatexKitsuneMaleModel::createBodyLayer);
         event.registerLayerDefinition(LatexKitsuneFemaleModel.LAYER_LOCATION, LatexKitsuneFemaleModel::createBodyLayer);
-
-        //Accessories
-        event.registerLayerDefinition(AccessoriesFemaleWolf.LAYER_LOCATION, AccessoriesFemaleWolf::createBodyLayer);
-        event.registerLayerDefinition(AccessoriesMaleWolf.LAYER_LOCATION, AccessoriesMaleWolf::createBodyLayer);
-        event.registerLayerDefinition(AccessoriesStiger.LAYER_LOCATION, AccessoriesStiger::createBodyLayer);
-        event.registerLayerDefinition(AccessoriesMaleFox.LAYER_LOCATION, AccessoriesMaleFox::createBodyLayer);
-        event.registerLayerDefinition(AccessoriesFemaleWolf.LAYER_LOCATION, AccessoriesFemaleFox::createBodyLayer);
-
 
         //Armors Models
         event.registerLayerDefinition(DarkLatexCoatModel.LAYER_LOCATION, DarkLatexCoatModel::createBodyLayer);
