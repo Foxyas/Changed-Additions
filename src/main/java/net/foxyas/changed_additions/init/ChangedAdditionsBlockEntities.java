5565916761b6f31f1963bced86393c841c3d934c
Foxyas <119010731+Foxyas@users.noreply.github.com>
2025-10-04 23:06:25 -0300

    Update ChangedAdditionsBlockEntities.java

--- src/main/java/net/foxyas/changed_additions/init/ChangedAdditionsBlockEntities.java
+++ src/main/java/net/foxyas/changed_additions/init/ChangedAdditionsBlockEntities.java
@@ -10,10 +10,10 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ChangedAdditionsBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, ChangedAdditionsMod.MODID);
+    public static final RegistryObject<BlockEntityType<?>> NEOFUSER = register("neofuser", ChangedAdditionsBlocks.NEOFUSER, NeofuserBlockEntity::new);
 
     private static RegistryObject<BlockEntityType<?>> register(String registryname, RegistryObject<Block> block, BlockEntityType.BlockEntitySupplier<?> supplier) {
         return REGISTRY.register(registryname, () -> BlockEntityType.Builder.of(supplier, block.get()).build(null));
-    }    public static final RegistryObject<BlockEntityType<?>> NEOFUSER = register("neofuser", ChangedAdditionsBlocks.NEOFUSER, NeofuserBlockEntity::new);
-
+    }   
 
 }
